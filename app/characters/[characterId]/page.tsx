64bdabf6b6ec918a50d1ec35cd58c3eae36b9d0f
“onango” <atnodongo@gmail.com>
2024-02-20 18:41:13 +0300

    Added character page

---
+++ app/characters/[characterId]/page.tsx
@@ -0,0 +1,13 @@
+const CharacterPage = ({
+  params: { characterId },
+}: {
+  params: { characterId: string };
+}) => {
+  
+  return (
+    <div >
+    </div>
+  );
+};
+
+export default CharacterPage;
