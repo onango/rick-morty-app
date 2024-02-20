055489a00a8ac5e6024afac33a6eea504969447f
“onango” <atnodongo@gmail.com>
2024-02-20 17:12:12 +0300

    updated character search result component

--- app/screens/SearchResultScreen/SearchResultScreen.tsx
+++ app/screens/SearchResultScreen/SearchResultScreen.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import { FC } from "react";
+import { CharacterCard } from "@/app/components/CharacterCard";
+import { CharacterRequest } from "@/types/api.types";
 
 interface SearchResultScreenProps {
   data: CharacterRequest;
@@ -7,13 +10,23 @@ interface SearchResultScreenProps {
 }
 
 const SearchResultScreen: FC<SearchResultScreenProps> = ({
+  currentPage,
   data: {
+    results,
     info: { pages },
   },
+  gender,
 }) => {
   return (
     <div className="container mx-auto">
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {results.map((character) => {
+          return (
+            <Link href={`/characters/${character.id}`} key={character.id}>
+              <CharacterCard type="small" character={character} />
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
