3038790d3b13b79b813ae1bf6edd7ebe96581011
“onango” <atnodongo@gmail.com>
2024-02-20 16:53:37 +0300

    Addd get characters fn to fetch characters from api

---
+++ utils/getCharacters.ts
@@ -0,0 +1,15 @@
+export const getCharacters = async (
+  page: number = 1,
+  gender?: string,
+  name?: string
+) => {
+  var url = new URL("https://rickandmortyapi.com/api/character/");
+  page && url.searchParams.set("page", page.toString());
+  gender && url.searchParams.set("gender", gender);
+  name && url.searchParams.set("name", name);
+
+  const res = await fetch(url.href, { cache: "no-store" });
+  
+  console.log(res);
+};
+
