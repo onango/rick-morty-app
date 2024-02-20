e205238c78638a05b3bb4f5d1b3ab76fce361cb9
“onango” <atnodongo@gmail.com>
2024-02-20 17:07:24 +0300

    Added search bar and data on component

--- app/locations/[locationId]/page.tsx
+++ app/locations/[locationId]/page.tsx
@@ -1,15 +1,31 @@
 import { NavBar } from "@/app/components/NavBar";
+import { Spinner } from "@/app/components/Spinner";
+import { CharacterSearchBar } from "@/app/components/CharacterSearchBar";
+import { SearchResultScreen } from "@/app/screens/SearchResultScreen";
+import { getCharacters } from "@/utils/getCharacters";
+import { Suspense } from "react";
 
 export default async function Home({
+  searchParams,
 }: {
   params: { slug: string };
   searchParams?: Record<string, string> | URLSearchParams | undefined;
 }) {
+  const urlSearchParams = new URLSearchParams(searchParams);
+  const pageNumber = Number(urlSearchParams.get("page")) || 1;
+  const gender = urlSearchParams.get("gender") || undefined;
+  const characterName = urlSearchParams.get("name") || undefined;
+  const { ok, data } = await getCharacters(pageNumber, gender, characterName);
 
   return (
     <main>
       <div>
         <NavBar />
+        <div className="h-full bg-no-repeat">
+          <div className="mt-10  px-10">
+            <CharacterSearchBar characterName={characterName} />
+          </div>
+        </div>
       </div>
     </main>
   );
