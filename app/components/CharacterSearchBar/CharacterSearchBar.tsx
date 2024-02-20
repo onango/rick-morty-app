673f42cce29ec7f64d2f127f5dbbe0d8b9a84e7f
“onango” <atnodongo@gmail.com>
2024-02-20 16:45:15 +0300

    Added form on search bar

--- app/components/CharacterSearchBar/CharacterSearchBar.tsx
+++ app/components/CharacterSearchBar/CharacterSearchBar.tsx
@@ -9,7 +9,39 @@ interface SearchBarProps {
 const CharacterSearchBar: FC<SearchBarProps> = ({ characterName }) => {
   return (
     <div className="flex h-12 w-full">
-    </div>
+    <form className="flex">
+      <input
+        autoComplete="off"
+        type="search"
+        id="searchByName"
+        name="search"
+        className="h-full w-full pl-5 text-sm  bg-transparent border border-r  border-gray-700 placeholder-gray-400 text-black focus:border-gray-500 outline-none  "
+        placeholder="Search character by name..."
+        required
+      />
+      <button
+        type="submit"
+        className="flex w-14 items-center justify-center text-sm font-medium text-black  rounded-r-lg border border-gray-700 hover:border-gray-800 focus:ring-4 focus:outline-none "
+      >
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+        <span className="sr-only">Search</span>
+      </button>
+    </form>
+  </div>
   );
 };
 
