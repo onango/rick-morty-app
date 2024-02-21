5cbd20bd889fb83d26151783c864824514ac98fb
“onango” <atnodongo@gmail.com>
2024-02-21 09:07:43 +0300

    Refactor component to fix hydration error and ensuring proper hydration and rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,38 @@
-import { Suspense } from "react";
-import { NavBar } from "@/app/components/NavBar";
-import {Spinner} from "@/app/components/Spinner";
-import { LocationSearchBar } from "@/app/components/LocationSearchBar";
-import { LocationResultScreen } from "@/app/screens/LocationResultScreen";
-import { getLocations } from "@/utils/getLocations";
+"use client";
 
+import { useEffect, useState } from 'react';
+import { NavBar } from '@/app/components/NavBar';
+import { Spinner } from '@/app/components/Spinner';
+import { LocationSearchBar } from '@/app/components/LocationSearchBar';
+import { LocationResultScreen } from '@/app/screens/LocationResultScreen';
+import { getLocations } from '@/utils/getLocations';
 
-export default async function Home({
-  searchParams,
-}: {
-  params: { slug: string };
-  searchParams?: Record<string, string> | URLSearchParams | undefined;
-}) {
+export default function Home({ searchParams }: { searchParams?: Record<string, string> | URLSearchParams | undefined }) {
+  const [locations, setLocations] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const urlSearchParams = new URLSearchParams(searchParams);
   const pageNumber = Number(urlSearchParams.get("page")) || 1;
   const locationName = urlSearchParams.get("name") || undefined;
-  const { ok, data } = await getLocations(pageNumber, locationName);
-  console.log(data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { ok, data } = await getLocations(pageNumber, locationName);
+        if (ok && data) {
+          setLocations(data);
+        } else {
+          setLocations(null);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [searchParams]);
 
   return (
     <main>
@@ -25,18 +41,15 @@ export default async function Home({
         <div className="h-full">
           <div className="mt-10  px-10">
             <LocationSearchBar locationName={locationName} />
-            <Suspense fallback={<Spinner />}>
-              {ok && data ? (
-                <LocationResultScreen
-                  data={data}
-                  currentPage={pageNumber}
-                />
-              ) : (
-                <div className="mt-10 flex text-black flex-1 justify-center">
-                  <p>Location not found</p>
-                </div>
-              )}
-            </Suspense>
+            {loading ? (
+              <Spinner />
+            ) : locations ? (
+              <LocationResultScreen data={locations} currentPage={pageNumber} />
+            ) : (
+              <div className="mt-10 flex text-black flex-1 justify-center">
+                <p>Location not found</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
