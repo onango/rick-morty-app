b1c79a355e3d95353778e6494cc9639f551fe69a
“onango” <atnodongo@gmail.com>
2024-02-21 12:34:39 +0300

    Added character search form on component

--- app/locations/[locationId]/page.tsx
+++ app/locations/[locationId]/page.tsx
@@ -46,6 +46,40 @@ export default function Home({ params, searchParams }: { params: { locationId: s
         <NavBar />
         <div className="h-full bg-no-repeat">
           <div className="mt-10 px-10">
+            <div className="flex h-12 w-full">
+            <form className="w-full flex">
+              <input
+                autoComplete="off"
+                type="search"
+                id="searchByName"
+                name="search"
+                className="h-full w-full pl-5 text-sm  bg-transparent border border-r border-gray-700 placeholder-gray-400 text-black focus:border-gray-500 outline-none  "
+                placeholder="Search character by name..."
+                required
+              />
+              <button
+                type="submit"
+                className="flex w-14 items-center justify-center text-sm font-medium text-black  rounded-r-lg border border-gray-700 hover:border-gray-800 focus:ring-4 focus:outline-none "
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+                <span className="sr-only">Search</span>
+              </button>
+            </form>
+            </div>
             <CharacterSearchBar characterName={characterName} />
             <Suspense fallback={<Spinner />}>
               {loading ? (
